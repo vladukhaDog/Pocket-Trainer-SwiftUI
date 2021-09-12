7f715ac8283b8f943137aa0236671eb0f08275c2
vladukhaDog <vladic.p@gmail.com>
2021-09-13 00:42:25 +0300

    Initial Commit

---
+++ Pocket Trainer/Pocket_TrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pocket_TrainerApp.swift
+//  Pocket Trainer
+//
+//  Created by vladukha on 13.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct Pocket_TrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
